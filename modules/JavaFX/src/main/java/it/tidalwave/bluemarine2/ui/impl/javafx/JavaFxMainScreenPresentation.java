59d93da959442fe73cb5ac3609d8ebae5956e635
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-03-20 19:14:30 +0100

    Merged.

---
+++ modules/JavaFX/src/main/java/it/tidalwave/bluemarine2/ui/impl/javafx/JavaFxMainScreenPresentation.java
@@ -0,0 +1,20 @@
+/*
+ * #%L
+ * %%
+ * %%
+ * #L%
+ */
+
+package it.tidalwave.bluemarine2.ui.impl.javafx;
+
+import it.tidalwave.bluemarine2.ui.MainScreenPresentation;
+
+/***********************************************************************************************************************
+ *
+ * @author  fritz
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class JavaFxMainScreenPresentation implements MainScreenPresentation 
+  {
+  }
---
+++ modules/Model/src/main/java/it/tidalwave/bluemarine2/model/MediaFile.java
@@ -0,0 +1,19 @@
+/*
+ * #%L
+ * %%
+ * %%
+ * #L%
+ */
+
+package it.tidalwave.bluemarine2.model;
+
+/***********************************************************************************************************************
+ *
+ * @author  fritz
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public interface MediaFile 
+  {
+    public static final Class<MediaFile> MediaFile = MediaFile.class;
+  }
