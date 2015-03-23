d318ebad446093a6b79b86e36813296fb77eca52
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-03-23 23:16:51 +0100

    Merged.

---
+++ modules/Installers/LinuxEmbeddedJRE/src/main/resources/blueMarine2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CLASSPATH=""
+SEPARATOR=""
+
+for i in lib/*.jar; do
+	CLASSPATH="$CLASSPATH$SEPARATOR$i"
+        SEPARATOR=":"
+	done
+
+java -cp "$CLASSPATH" it.tidalwave.bluemarine2.ui.impl.javafx.Main
