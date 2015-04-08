4edb7c8a53b709cd10d288ac976629529bad09c7
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-04-08 23:15:19 +0200

    Cleaned up.

--- modules/Catalog/src/main/java/it/tidalwave/bluemarine2/catalog/impl/RepositoryMusicArtistFinder.java
+++ modules/Catalog/src/main/java/it/tidalwave/bluemarine2/catalog/impl/RepositoryMusicArtistFinder.java
@@ -61,20 +61,29 @@ public class RepositoryMusicArtistFinder extends RepositoryFinderSupport<MusicAr
     @Override @Nonnull
     protected List<? extends MusicArtist> computeNeededResults() 
       {
-          // TODO includere anche quelli con type == 2 e nessuna collaborazione (e.g. Rondo Veneziano: gruppi senza elenco partecipanti 
         return query(RepositoryMusicArtistEntity.class,  
               "SELECT *"
             + "WHERE  {\n" 
             + "       ?artist a                 mo:MusicArtist.\n" 
             + "       ?artist rdfs:label        ?label.\n" 
-                      // FIXME: può non esserci. Metti <2 oppure non esiste
-            + "      { ?artist vocab:artist_type \"1\"^^xs:short. }\n"
+                // Single artists
+            + "         {\n"
+            + "            ?artist vocab:artist_type \"1\"^^xs:short.\n"
+            + "         }\n"
             + "       UNION\n"
-            + "      { ?artist vocab:artist_type \"2\"^^xs:short. \n"
-            + "         FILTER NOT EXISTS { ?artist rel:collaboratesWith ?any } }\n"
-//            + "      { FILTER NOT EXISTS { ?artist vocab:artist_type ?type } }\n"
-//            + "       OPTIONAL { ?artist vocab:artist_type \"1\"^^xs:short }"
+                // Groups for which there is no defined collaboration with single persons
+            + "         {\n"
+            + "             ?artist vocab:artist_type \"2\"^^xs:short.\n"
+            + "             FILTER NOT EXISTS { ?artist rel:collaboratesWith ?any }\n"
+            + "         }\n"
             + "       }\n" 
             + "ORDER BY ?label");
       }
+    
+                      // FIXME: può non esserci. Metti <2 oppure non esiste
+/*
+               + "       UNION\n"
+            + "      { FILTER NOT EXISTS { ?artist vocab:artist_type ?type } }\n"
+
+    */    
   }
