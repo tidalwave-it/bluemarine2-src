6ccbb08d46b78fdf61e97a04850d2981859660fd
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-04-13 14:00:26 +0200

    ...

--- modules/MediaScanner/src/main/java/it/tidalwave/bluemarine2/mediascanner/impl/EmbeddedMetadataManager.java
+++ modules/MediaScanner/src/main/java/it/tidalwave/bluemarine2/mediascanner/impl/EmbeddedMetadataManager.java
@@ -75,6 +75,7 @@ public class EmbeddedMetadataManager
     class ConcurrentHashMapWithOptionals<K, V> extends ConcurrentHashMap<K, V>
       {
         // FIXME: rename (semantics is slightly different than the original putIfAbsent())
+        @Nonnull
         public Optional<K> putIfAbsentAndGetNewKey (final @Nonnull Optional<K> key, final @Nonnull V value)
           {
             return (key.isPresent() && (putIfAbsent(key.get(), value) == null)) ? key : Optional.empty();
