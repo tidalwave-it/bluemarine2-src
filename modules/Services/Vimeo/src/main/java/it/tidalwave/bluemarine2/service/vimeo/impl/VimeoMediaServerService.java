d71b26d2e87a3da74717547737ab23213b02c761
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-04-25 19:37:14 +0200

    First work on BMT-59.

---
+++ modules/Services/Vimeo/src/main/java/it/tidalwave/bluemarine2/service/vimeo/impl/VimeoMediaServerService.java
@@ -0,0 +1,64 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueMarine2 - Semantic Media Center
+ * http://bluemarine2.tidalwave.it - git clone https://tidalwave@bitbucket.org/tidalwave/bluemarine2-src.git
+ * %%
+ * Copyright (C) 2015 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.bluemarine2.service.vimeo.impl;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Collection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import it.tidalwave.bluemarine2.model.MediaFolder;
+import it.tidalwave.bluemarine2.model.spi.VirtualMediaFolder;
+import it.tidalwave.bluemarine2.mediaserver.spi.MediaServerService;
+import it.tidalwave.bluemarine2.model.Entity;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class VimeoMediaServerService implements MediaServerService
+  {
+    private static final Path PATH_ROOT = Paths.get("vimeo");
+
+    @Override @Nonnull
+    public MediaFolder createRootFolder (final @Nonnull MediaFolder parent)
+      {
+        return new VirtualMediaFolder(parent, PATH_ROOT, "Vimeo", this::childrenFactory);
+      }
+
+    @Nonnull
+    private Collection<Entity> childrenFactory (final @Nonnull MediaFolder parent)
+      {
+        return Arrays.asList();
+//        return Arrays.asList(new VirtualMediaFolder(parent, PATH_DIARY,  "Diary",  diaryProvider::findPhotos),
+//                             new VirtualMediaFolder(parent, PATH_THEMES, "Themes", themesProvider::findPhotos));
+      }
+  }
---
+++ modules/Services/Vimeo/src/test/java/it/tidalwave/bluemarine2/service/vimeo/impl/VimeoMediaServerServiceTest.java
@@ -0,0 +1,55 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueMarine2 - Semantic Media Center
+ * http://bluemarine2.tidalwave.it - git clone https://tidalwave@bitbucket.org/tidalwave/bluemarine2-src.git
+ * %%
+ * Copyright (C) 2015 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.bluemarine2.service.vimeo.impl;
+
+import it.tidalwave.bluemarine2.upnp.mediaserver.impl.ClingTestSupport;
+import org.testng.annotations.Test;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class VimeoMediaServerServiceTest extends ClingTestSupport
+  {
+    public VimeoMediaServerServiceTest()
+      {
+        super("classpath*:META-INF/VimeoServiceAutoBeans.xml",
+              "classpath*:META-INF/MediaServerAutoBeans.xml",
+              "classpath*:META-INF/UPnPAutoBeans.xml");
+      }
+
+    @Test
+    public void test_deploy()
+      throws InterruptedException
+      {
+        delay();
+      }
+  }
