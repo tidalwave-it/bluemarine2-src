84bdadeaae581434de9b38cb37112c4509f54960
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-04-28 21:13:22 +0200

    Fixed broken test.

--- modules/MediaServer/src/test/java/it/tidalwave/bluemarine2/mediaserver/impl/DefaultContentDirectoryTest.java
+++ modules/MediaServer/src/test/java/it/tidalwave/bluemarine2/mediaserver/impl/DefaultContentDirectoryTest.java
@@ -66,7 +66,7 @@ public class DefaultContentDirectoryTest
         final List<? extends Entity> children = root.findChildren().results();
         assertThat(children.size(), is(4));
         assertThat(children.get(0).as(Displayable).getDisplayName(), is("Music"));
-        assertThat(children.get(1).as(Displayable).getDisplayName(), is("Photo"));
+        assertThat(children.get(1).as(Displayable).getDisplayName(), is("Photos"));
         assertThat(children.get(2).as(Displayable).getDisplayName(), is("Video"));
         assertThat(children.get(3).as(Displayable).getDisplayName(), is("Services"));
       }
