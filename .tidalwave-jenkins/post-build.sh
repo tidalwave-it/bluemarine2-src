0cadcb688666282f7504179155c0cdc255cbf0fc
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-12-18 20:54:25 +0100

    ...

--- .tidalwave-jenkins/post-build.sh
+++ .tidalwave-jenkins/post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Hudson uses java.io.File API to remve and archive files in the workspace,
+# Hudson uses thejava.io.File API to remove and archive files in the workspace,
 # so it fails with file names containing diacritics. We have to manually remove them.
 
 if [ -z ${WORKSPACE+x} ]; then 
