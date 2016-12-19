49af72389ee44855716595f4c8fa8741174c97db
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-12-19 21:56:40 +0100

    Improved script.

--- modules/Model/src/scripts/create-expected-results.sh
+++ modules/Model/src/scripts/create-expected-results.sh
@@ -4,8 +4,15 @@ set -e
 
 function usage()
   {
-    echo "Usage: create-expected-results test_set_name version"
+    echo "Usage: create-expected-results [--deploy] test_set_name version"
   }
+
+GOAL=install:install-file
+
+if [ "$1" == "--deploy" ] ; then
+    GOAL=gpg:sign-and-deploy-file
+    shift
+    fi
 
 readonly TEST_SET=$1
 readonly VERSION=$2
@@ -31,7 +38,36 @@ if [ ! -d "$TEST_RESULTS/$TEST_SET" ] ; then
 
 rm -vf $FILE
 
-cd $TEST_RESULTS ; zip -prq $FILE $TEST_SET/*
+(cd $TEST_RESULTS ; zip -prq $FILE $TEST_SET/*)
 
+cat << EOF > $PWD/target/thepom.xml
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
 
-mvn install:install-file -Dfile=$FILE -DgeneratePom=true -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=zip
+    <modelVersion>4.0.0</modelVersion>
+
+    <parent>
+        <groupId>it.tidalwave.bluemarine2</groupId>
+        <artifactId>blueMarine2</artifactId>
+        <version>1.0-ALPHA-10</version>
+        <relativePath/>
+    </parent>
+
+    <groupId>$GROUP_ID</groupId>
+    <artifactId>$ARTIFACT_ID</artifactId>
+    <version>$VERSION</version>
+    <packaging>zip</packaging>
+
+</project>
+EOF
+
+mvn $GOAL \
+    -Dfile=$FILE \
+    -DpomFile=$PWD/target/thepom.xml \
+    -DcreateChecksum=true \
+    -DgroupId=$GROUP_ID \
+    -DartifactId=$ARTIFACT_ID \
+    -Dversion=$VERSION \
+    -Dpackaging=zip \
+    -Dtft.maven-gpg-plugin.version=1.6 \
+    -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 \
+    -DrepositoryId=staging-oss.sonatype.org
