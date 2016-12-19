bd332da077b81a63618d4dd9c788413b5e04d19c
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-12-19 21:23:19 +0100

    Improved script.

--- modules/Model/src/scripts/create-expected-results.sh
+++ modules/Model/src/scripts/create-expected-results.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-readonly FILE=/tmp/artifact.zip
+function usage()
+  {
+    echo "Usage: create-expected-results test_set_name version"
+  }
+
 readonly TEST_SET=$1
 readonly VERSION=$2
 
-rm -v $FILE
+readonly FILE=$PWD/target/artifact.zip
+readonly TEST_RESULTS=target/test-results
 
-cd target/test-results/ ; zip -prq $FILE $TEST_SET/*
+if [ ! -d "$TEST_RESULTS/$TEST_SET" ] ; then
+    readonly TEST_SETS=`cd $TEST_RESULTS; echo *`
+    echo "Invalid test set name, pick one in:"
+    echo ""
+
+    for i in  $TEST_SETS; do
+        echo "            $i"
+        done
+
+    echo ""
+    usage
+    exit 1
+    fi
+
+rm -vf $FILE
+
+cd $TEST_RESULTS ; zip -prq $FILE $TEST_SET/*
 
 readonly GROUP_ID=it.tidalwave.bluemarine2.testsets
 readonly ARTIFACT_ID=expected-metadata-$TEST_SET
