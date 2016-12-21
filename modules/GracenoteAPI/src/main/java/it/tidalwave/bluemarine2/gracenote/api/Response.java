0e66256d4d49ce174bc83a5bc7515e674e0af6ee
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-12-21 13:20:45 +0100

    Commented.

--- modules/GracenoteAPI/src/main/java/it/tidalwave/bluemarine2/gracenote/api/Response.java
+++ modules/GracenoteAPI/src/main/java/it/tidalwave/bluemarine2/gracenote/api/Response.java
@@ -29,6 +29,7 @@
 package it.tidalwave.bluemarine2.gracenote.api;
 
 import javax.annotation.Nonnull;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Function;
 import java.io.ByteArrayInputStream;
@@ -52,6 +53,9 @@ import static lombok.AccessLevel.PRIVATE;
 
 /***********************************************************************************************************************
  *
+ * This class encapsulates a response from the Gracenote API, including the requested datum - if available - and some
+ * status codes.
+ *
  * @author  Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
  * @version $Id: $
  *
@@ -69,31 +73,75 @@ public class Response<T>
     @Getter @Nonnull
     private final String responseStatus;
 
-    // FIXME: add error code
-
+    /*******************************************************************************************************************
+     *
+     * Returns the datum, if available.
+     *
+     * @return      the datum
+     * @throws      NoSuchElementException if the datum is not available
+     *
+     ******************************************************************************************************************/
     @Nonnull
     public T get()
+      throws NoSuchElementException
       {
         return datum.get();
       }
 
+    /*******************************************************************************************************************
+     *
+     * Returns <code>true</code> if the datum is available.
+     *
+     * @return      <code>true</code> if the datum is available
+     *
+     ******************************************************************************************************************/
     public boolean isPresent()
       {
         return datum.isPresent();
       }
 
+    /*******************************************************************************************************************
+     *
+     * Maps the result, if present, to a new value using a mapping function.
+     *
+     * @param <U>       the type of the result
+     * @param mapper    a mapping function to apply to the value, if present
+     * @return          an {@code Optional} result
+     *
+     ******************************************************************************************************************/
     @Nonnull
-    public <U> Optional<U> map (final @Nonnull Function<? super T, ? extends U> function)
+    public <U> Optional<U> map (final @Nonnull Function<? super T, ? extends U> mapper)
       {
-        return datum.map(function);
+        return datum.map(mapper);
       }
 
+    /*******************************************************************************************************************
+     *
+     * Maps the result, if present, to a new value using a mapping function, avoiding wrapping with multiple
+     * {@code Optional}s.
+     *
+     * @param <U>       the type of the result
+     * @param mapper    a mapping function to apply to the value, if present
+     * @return          an {@code Optional} result
+     *
+     ******************************************************************************************************************/
     @Nonnull
-    public <U> Optional<U> flatMap (final @Nonnull Function<? super T, Optional<U>> function)
+    public <U> Optional<U> flatMap (final @Nonnull Function<? super T, Optional<U>> mapper)
       {
-        return datum.flatMap(function);
+        return datum.flatMap(mapper);
       }
 
+    /*******************************************************************************************************************
+     *
+     * Creates a {@code Response} containing a datum out of a {@link ResponseEntity} applying a parser. The parser
+     * receives an XML DOM as the input - it typically uses XPath to extract information.
+     *
+     * @param <X>       the type of the datum
+     * @param response  the HTTP response
+     * @param parser    the parser that produces the datum
+     * @return          the {@code Response}
+     *
+     ******************************************************************************************************************/
     @Nonnull
     public static <X> Response<X> of (final @Nonnull ResponseEntity<String> response,
                                       final @Nonnull Function<Document, X> parser)
@@ -131,6 +179,11 @@ public class Response<T>
           }
       }
 
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
     @Nonnull
     private static Document toDom (final @Nonnull ResponseEntity<String> response)
       {
