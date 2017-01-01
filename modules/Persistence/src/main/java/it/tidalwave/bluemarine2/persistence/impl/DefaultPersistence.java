ef1c78ef0855816015ff0d93dfdad81faaba8bd4
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-01-01 21:25:58 +0100

    Refactored.

--- modules/Persistence/src/main/java/it/tidalwave/bluemarine2/persistence/impl/DefaultPersistence.java
+++ modules/Persistence/src/main/java/it/tidalwave/bluemarine2/persistence/impl/DefaultPersistence.java
@@ -30,7 +30,6 @@ package it.tidalwave.bluemarine2.persistence.impl;
 
 import javax.annotation.Nonnull;
 import java.util.concurrent.CountDownLatch;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
@@ -53,7 +52,6 @@ import it.tidalwave.messagebus.annotation.SimpleMessageSubscriber;
 import it.tidalwave.bluemarine2.util.PowerOnNotification;
 import it.tidalwave.bluemarine2.persistence.Persistence;
 import it.tidalwave.bluemarine2.persistence.PropertyNames;
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -97,16 +95,15 @@ public class DefaultPersistence implements Persistence
         try
           {
             final Path repositoryPath = notification.getProperties().get(PropertyNames.REPOSITORY_PATH);
-            final RepositoryConnection connection = repository.getConnection();
 
             if (Files.exists(repositoryPath))
               {
-                try (final Reader reader = Files.newBufferedReader(repositoryPath, UTF_8))
+                try (final RepositoryConnection connection = repository.getConnection();
+                     final Reader reader = Files.newBufferedReader(repositoryPath, UTF_8))
                   {
                     log.info("Importing repository from {} ...", repositoryPath);
                     connection.add(reader, repositoryPath.toUri().toString(), RDFFormat.N3);
                     connection.commit();
-                    connection.close();
                   }
               }
           }
@@ -127,30 +124,31 @@ public class DefaultPersistence implements Persistence
       throws RDFHandlerException, IOException, RepositoryException
       {
         log.info("dump({})", path);
-        final File file = path.toFile();
-        file.getParentFile().mkdirs();
-        @Cleanup final PrintWriter pw = new PrintWriter(file, "UTF-8");
-        final RDFHandler writer = new SortingRDFHandler(new N3Writer(pw));
-        final RepositoryConnection connection = repository.getConnection();
+        Files.createDirectories(path.getParent());
 
-        for (final Namespace namespace : connection.getNamespaces().asList())
+        try (final PrintWriter pw = new PrintWriter(Files.newBufferedWriter(path, UTF_8));
+             final RepositoryConnection connection = repository.getConnection())
           {
-            writer.handleNamespace(namespace.getPrefix(), namespace.getName());
+            final RDFHandler writer = new SortingRDFHandler(new N3Writer(pw));
+
+            for (final Namespace namespace : connection.getNamespaces().asList())
+              {
+                writer.handleNamespace(namespace.getPrefix(), namespace.getName());
+              }
+
+            writer.handleNamespace("bio",   "http://purl.org/vocab/bio/0.1/");
+            writer.handleNamespace("bmmo",  "http://bluemarine.tidalwave.it/2015/04/mo/");
+            writer.handleNamespace("dc",    "http://purl.org/dc/elements/1.1/");
+            writer.handleNamespace("foaf",  "http://xmlns.com/foaf/0.1/");
+            writer.handleNamespace("owl",   "http://www.w3.org/2002/07/owl#");
+            writer.handleNamespace("mo",    "http://purl.org/ontology/mo/");
+            writer.handleNamespace("rdfs",  "http://www.w3.org/2000/01/rdf-schema#");
+            writer.handleNamespace("rel",   "http://purl.org/vocab/relationship/");
+            writer.handleNamespace("vocab", "http://dbtune.org/musicbrainz/resource/vocab/");
+            writer.handleNamespace("xs",    "http://www.w3.org/2001/XMLSchema#");
+
+            connection.export(writer);
           }
-
-        writer.handleNamespace("bio",   "http://purl.org/vocab/bio/0.1/");
-        writer.handleNamespace("bmmo",  "http://bluemarine.tidalwave.it/2015/04/mo/");
-        writer.handleNamespace("dc",    "http://purl.org/dc/elements/1.1/");
-        writer.handleNamespace("foaf",  "http://xmlns.com/foaf/0.1/");
-        writer.handleNamespace("owl",   "http://www.w3.org/2002/07/owl#");
-        writer.handleNamespace("mo",    "http://purl.org/ontology/mo/");
-        writer.handleNamespace("rdfs",  "http://www.w3.org/2000/01/rdf-schema#");
-        writer.handleNamespace("rel",   "http://purl.org/vocab/relationship/");
-        writer.handleNamespace("vocab", "http://dbtune.org/musicbrainz/resource/vocab/");
-        writer.handleNamespace("xs",    "http://www.w3.org/2001/XMLSchema#");
-
-        connection.export(writer);
-        connection.close();
       }
 
     /*******************************************************************************************************************
