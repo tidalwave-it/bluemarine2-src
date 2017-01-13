123732b7c39f204398a88aa77512312ab6238153
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-01-13 11:58:22 +0100

    Cleaned up, commented.

--- modules/Model/src/main/java/it/tidalwave/bluemarine2/model/impl/PathAwareEntityFinderDelegate.java
+++ modules/Model/src/main/java/it/tidalwave/bluemarine2/model/impl/PathAwareEntityFinderDelegate.java
@@ -38,7 +38,6 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Function;
 import java.nio.file.Path;
-import org.springframework.beans.factory.annotation.Configurable;
 import it.tidalwave.util.As;
 import it.tidalwave.util.Finder;
 import it.tidalwave.util.Finder8;
@@ -59,8 +58,8 @@ import static lombok.AccessLevel.PRIVATE;
  * A decorator of an {@link Finder} of {@link PathAwareEntity} that creates a virtual tree of entities. Each entity is
  * given a path, which starts with the path of a {@link MediaFolder} and continues with the id of the entity.
  *
- * This {@code Finder} can filter by path. If a filter path is provided, the filtering happens in memory: this means
- * that if the delegate queries a repository, all the data are first retrieve in memory.
+ * This {@code Finder} can filtered by path. If a filter path is provided, the filtering happens in memory: this means
+ * that even when the delegate queries a native store, all the data are first retrieved in memory.
  *
  * @stereotype  Finder
  *
@@ -68,7 +67,7 @@ import static lombok.AccessLevel.PRIVATE;
  * @version $Id$
  *
  **********************************************************************************************************************/
-@RequiredArgsConstructor(access = PRIVATE) @Configurable @Slf4j
+@RequiredArgsConstructor(access = PRIVATE) @Slf4j
 public class PathAwareEntityFinderDelegate extends Finder8Support<PathAwareEntity, EntityFinder> implements EntityFinder
   {
     private static final long serialVersionUID = 4429676480224742813L;
@@ -84,7 +83,12 @@ public class PathAwareEntityFinderDelegate extends Finder8Support<PathAwareEntit
 
     /*******************************************************************************************************************
      *
-     * Default constructor.
+     * Creates an instance associated to a given {@link MediaFolder} and a delegate finder.
+     *
+     * @see #PathAwareEntityFinderDelegate(it.tidalwave.bluemarine2.model.MediaFolder, java.util.function.Function)
+     *
+     * @param   mediaFolder     the folder associated to this finder
+     * @param   delegate        the delegate finder to provide data
      *
      ******************************************************************************************************************/
     public PathAwareEntityFinderDelegate (final @Nonnull MediaFolder mediaFolder,
@@ -95,7 +99,18 @@ public class PathAwareEntityFinderDelegate extends Finder8Support<PathAwareEntit
 
     /*******************************************************************************************************************
      *
-     * Default constructor.
+     * Creates an instance associated to a given {@link MediaFolder} and a function for providing children. This
+     * constructor is typically used when the children are already present in memory (e.g. they are
+     * {@link VirtualMediaFolder}s. Because the function doesn't have the full semantics of a {@link Finder} - it can't
+     * optimise a query in function of search parameters, nor optimise the count of results - when a
+     * {@code PathAwareEntityFinderDelegate} is created in this way all operations will be performed in memory. If one
+     * can provide data from a native store and enjoy optimised queries, instead of this constructor use
+     * {@link #PathAwareEntityFinderDelegate(it.tidalwave.bluemarine2.model.MediaFolder, it.tidalwave.util.Finder8)}
+     *
+     * @see #PathAwareEntityFinderDelegate(it.tidalwave.bluemarine2.model.MediaFolder, it.tidalwave.util.Finder8)
+     *
+     * @param   mediaFolder     the folder associated to this finder
+     * @param   function        the function that provides children
      *
      ******************************************************************************************************************/
     public PathAwareEntityFinderDelegate (final @Nonnull MediaFolder mediaFolder,
@@ -161,22 +176,27 @@ public class PathAwareEntityFinderDelegate extends Finder8Support<PathAwareEntit
      *
      ******************************************************************************************************************/
     @Nonnull
-    private Optional<PathAwareEntity> filteredByPath (final @Nonnull Path path)
+    private Optional<? extends PathAwareEntity> filteredByPath (final @Nonnull Path path)
       {
-        // Cannot be optimized as a native query: the path concept in PathAwareEntity is totally decoupled from
-        // the underlying native store.
         log.debug("filteredByPath({})", path);
         return mediaFolder.getPath().equals(path)
-                ? Optional.of(mediaFolder)
-                : childMatchingPathHead(path).flatMap(entity -> path.equals(entity.getPath())
-                        ? Optional.of(entity)
-                        : childMatchingPath(entity, path));
+                                        ? Optional.of(mediaFolder)
+                                        : childMatchingPathHead(path).flatMap(entity -> path.equals(entity.getPath())
+                                                ? Optional.of(entity)
+                                                : childMatchingPath(entity, path));
       }
 
     /*******************************************************************************************************************
      *
      * Returns the child entity that matches the first element of the path, if present. The path can be exactly the one
      * of the found entity, or it can be of one of its children.
+     *
+     * This method performs a bulk query of all children and then filters by path in memory. It is not possible to
+     * use a query to the native store for the path - which would be good for performance reasons - , because even
+     * though each segment of the path is function of some attribute of the related {@code PathAwareEntity} - typically
+     * the id - it is not a matter of the native store. Performance of this section relies upon memory caching. Some
+     * experiment showed that it's not useful to add another caching layer here, and the one in
+     * {@code RepositoryFinderSupport} is enough.
      *
      * @param   path    the path
      * @return          the entity, if present
@@ -186,7 +206,7 @@ public class PathAwareEntityFinderDelegate extends Finder8Support<PathAwareEntit
     private Optional<PathAwareEntity> childMatchingPathHead (final @Nonnull Path path)
       {
 //                assert filtered.size() == 1 or 0;
-        log.debug(">>>> bulk query to {}, filtering in memory", delegate); // See BMT-128
+        log.debug(">>>> bulk query to {}, filtering in memory", delegate);
         return (Optional<PathAwareEntity>)delegate.results().stream()
                                                   .filter(entity -> sameHead(relative(path), relative(entity.getPath())))
                                                   .findFirst();
