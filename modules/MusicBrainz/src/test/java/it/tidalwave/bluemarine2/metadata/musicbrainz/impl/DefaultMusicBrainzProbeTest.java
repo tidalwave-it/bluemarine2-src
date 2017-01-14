ec1b62d416c3b94ba89880435b0891222d0e8a90
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-01-14 23:29:16 +0100

    Restored all tests.

--- modules/MusicBrainz/src/test/java/it/tidalwave/bluemarine2/metadata/musicbrainz/impl/DefaultMusicBrainzProbeTest.java
+++ modules/MusicBrainz/src/test/java/it/tidalwave/bluemarine2/metadata/musicbrainz/impl/DefaultMusicBrainzProbeTest.java
@@ -199,7 +199,7 @@ public class DefaultMusicBrainzProbeTest extends TestSupport
                 .filter(triple -> !triple.getFilePath().toString().contains("Compilations/Rachmaninov_ Piano Concertos #2 & 3"))
                 .filter(triple -> triple.getFilePath().getFileName().toString().startsWith("01"))
 
-                .filter(triple -> triple.getTestSetName().equals("iTunes-fg-20160504-1"))
+//                .filter(triple -> triple.getTestSetName().equals("iTunes-fg-20160504-1"))
 //                .filter(triple -> triple.getTestSetName().equals("iTunes-fg-20161210-1"))
 //                .filter(triple -> triple.getFilePath().toString().contains("Trio 99_00"))
 //                .filter(triple -> triple.getFilePath().toString().contains("La Divina 2"))
