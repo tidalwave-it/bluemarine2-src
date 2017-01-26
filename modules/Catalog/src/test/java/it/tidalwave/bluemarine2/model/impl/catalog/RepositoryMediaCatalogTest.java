65dfeeeaa4098a9de72925354319181519e2cbdb
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-01-26 16:10:53 +0100

    ...

--- modules/Catalog/src/test/java/it/tidalwave/bluemarine2/model/impl/catalog/RepositoryMediaCatalogTest.java
+++ modules/Catalog/src/test/java/it/tidalwave/bluemarine2/model/impl/catalog/RepositoryMediaCatalogTest.java
@@ -176,7 +176,8 @@ public class RepositoryMediaCatalogTest extends SpringTestSupport
             record.getGtin().ifPresent(gtin -> pw.printf("  BARCODE: %s%n", gtin));
 
             final TrackFinder recordTrackFinder = record.findTracks();
-            pw.printf("  TRACKS (%d / %s):%n", recordTrackFinder.count(), ((RepositoryRecord)record).getTrackCount());
+            pw.printf("  TRACKS (%d / %s):%n", recordTrackFinder.count(), ((RepositoryRecord)record).getTrackCount()); // FIXME: add getTrackCount() in Record
+//            ((RepositoryRecord)record).getTrackCount().ifPresent(trackCount -> assertEquals(trackCount.intValue(), recordTrackFinder.count())); FIXME
 
             recordTrackFinder.stream().forEach(track ->
               {
