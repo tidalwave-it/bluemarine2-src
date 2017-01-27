c8e3b7bb835d671986110cd1e53d2f6e79612655
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-01-28 00:43:23 +0100

    Partial fix for BMT-83: initial setup.

---
+++ modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/MusicResourcesController.java
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueMarine2 - Semantic Media Center
+ * http://bluemarine2.tidalwave.it - git clone https://bitbucket.org/tidalwave/bluemarine2-src.git
+ * %%
+ * Copyright (C) 2015 - 2017 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.bluemarine2.rest.impl;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
+ * @version $Id: Class.java,v 631568052e17 2013/02/19 15:45:02 fabrizio $
+ *
+ **********************************************************************************************************************/
+@RestController
+public class MusicResourcesController
+  {
+    // mvn jetty:run
+    // curl -v http://localhost:8080/it-tidalwave-bluemarine2-rest/dummy
+
+    // See https://spring.io/blog/2014/12/02/latest-jackson-integration-improvements-in-spring
+    //
+    @RequestMapping(value = "/dummy", produces=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Dummy getResource()
+      {
+        return new Dummy("foo", 4);
+      }
+  }
