0992196d69d7bdeb1c6dcf04d0dca040f0691f8e
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-02-09 19:39:49 +0100

    RangeServlet refactored.

--- modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/server/Range.java
+++ modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/server/Range.java
@@ -3,9 +3,9 @@
  * *********************************************************************************************************************
  *
  * blueMarine2 - Semantic Media Center
- * http://bluemarine2.tidalwave.it - git clone https://bitbucket.org/tidalwave/bluemarine2-src.git
+ * http://bluemarine2.tidalwave.it - git clone https://tidalwave@bitbucket.org/tidalwave/bluemarine2-src.git
  * %%
- * Copyright (C) 2015 - 2017 Tidalwave s.a.s. (http://tidalwave.it)
+ * Copyright (C) 2015 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  *
  * *********************************************************************************************************************
@@ -28,31 +28,195 @@
  */
 package it.tidalwave.bluemarine2.rest.impl.server;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 /***********************************************************************************************************************
  *
- * @author  Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
- * @version $Id: $
+ * @author  Fabrizio Giudici
+ * @version $Id$
  *
  **********************************************************************************************************************/
-class Range
+@Slf4j @Getter @EqualsAndHashCode
+public class Range
   {
-    public final long start;
-    public final long end;
-    public final long length;
-    public final long total;
+    private static final int DEFAULT_BUFFER_SIZE = 64 * 1024;
 
-    /**
+    private final long start;
+    private final long end;
+    private final long length;
+    private final long total;
+
+    /*******************************************************************************************************************
+     *
      * Construct a byte range.
-     * @param start Start of the byte range.
-     * @param end End of the byte range.
-     * @param total Total length of the byte source.
-     */
-    public Range (final long start, final long end, final long total)
+     *
+     * @param   start   start of the byte range.
+     * @param   end     end of the byte range.
+     * @param   total   total length of the byte source.
+     *
+     ******************************************************************************************************************/
+    public Range (final @Nonnegative long start, final @Nonnegative long end, final @Nonnegative long total)
       {
-        this.start  = start;
-        this.end    = end;
+        this.start = start;
+        this.end = end;
         this.length = end - start + 1;
-        this.total  = total;
+        this.total = total;
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public static Range full (final @Nonnegative long length)
+      {
+        return new Range(0, length - 1, length);
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public Range subrange (final @Nonnegative long size)
+      {
+       return new Range(start, Math.min(end, start + size - 1), total);
+      }
+
+    /*******************************************************************************************************************
+     *
+     * Parses a range from a HTTP header.
+     *
+     * @param   rangeHeader     the HTTP header
+     * @param   length          the length of the full datum
+     * @return                  the range
+     *
+     ******************************************************************************************************************/
+    @Nonnegative
+    public static List<Range> fromHeader (final @Nullable String rangeHeader, final @Nonnegative long length)
+      {
+        final List<Range> ranges = new ArrayList<>();
+
+        if (rangeHeader != null)
+          {
+            // Range header should match format "bytes=n-n,n-n,n-n...".
+            if (!rangeHeader.matches("^bytes=\\d*-\\d*(,\\d*-\\d*)*$"))
+              {
+                throw new IllegalArgumentException("Invalid range: " + rangeHeader);
+              }
+
+            // If any valid If-Range header, then process each part of byte range.
+            for (final String part : rangeHeader.substring(6).split(","))
+              {
+                // Assuming a file with length of 100, the following examples returns bytes at:
+                // 50-80 (50 to 80), 40- (40 to length=100), -20 (length-20=80 to length=100).
+                long start = subStringOrMinusOne(part, 0, part.indexOf("-"));
+                long end = subStringOrMinusOne(part, part.indexOf("-") + 1, part.length());
+
+                if (start == -1)
+                  {
+                    start = length - end;
+                    end = length - 1;
+                  }
+                else if ((end == -1) || (end > length - 1))
+                  {
+                    end = length - 1;
+                  }
+
+                if (start > end)
+                  {
+                    throw new IllegalArgumentException("Invalid range: " + rangeHeader);
+                  }
+
+                ranges.add(new Range(start, end, length));
+              }
+          }
+
+        return ranges;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * Copy the given byte range of the given input to the given output.
+     *
+     * @param   input           the input to copy the given range to the given output for.
+     * @param   output          the output to copy the given range from the given input for.
+     * @throws  IOException     if something fails at I/O level.
+     *
+     ******************************************************************************************************************/
+    public void copy (final @Nonnull RandomAccessFile input, final @Nonnull OutputStream output)
+      throws IOException
+      {
+                // FIXME: use memory mapped i/o
+        final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int read;
+
+        if (input.length() == length)
+          {
+            // Write full range.
+            while ((read = input.read(buffer)) > 0)
+              {
+                output.write(buffer, 0, read);
+              }
+          }
+        else
+          {
+            // Write partial range.
+            input.seek(start);
+            long toRead = length;
+
+            while ((read = input.read(buffer)) > 0)
+              {
+                if ((toRead -= read) > 0)
+                  {
+                    output.write(buffer, 0, read);
+                  }
+                else
+                  {
+                    output.write(buffer, 0, (int) toRead + read);
+                    break;
+                  }
+              }
+          }
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public String toHeader()
+      {
+        return "bytes " + start + "-" + end + "/" + total;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * Returns a substring of the given string value from the given begin index to the given end index as a long. If the
+     * substring is empty, then -1 will be returned
+     *
+     * @param   value       the string value to return a substring as long for.
+     * @param   beginIndex  the begin index of the substring to be returned as long.
+     * @param   endIndex    the end index of the substring to be returned as long.
+     * @return              a substring of the given string value as long or -1 if substring is empty.
+     *
+     ******************************************************************************************************************/
+    private static long subStringOrMinusOne (final @Nonnull String value,
+                                             final @Nonnegative int beginIndex,
+                                             final @Nonnegative int endIndex)
+      {
+        final String substring = value.substring(beginIndex, endIndex);
+        return (substring.length() > 0) ? Long.parseLong(substring) : -1;
       }
   }
 
--- modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/server/RangeServlet.java
+++ modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/server/RangeServlet.java
@@ -29,7 +29,6 @@
 package it.tidalwave.bluemarine2.rest.impl.server;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 import java.util.List;
 import java.io.EOFException;
 import java.io.IOException;
@@ -57,9 +56,7 @@ import static it.tidalwave.bluemarine2.util.Miscellaneous.normalizedPath;
 @RequiredArgsConstructor @Slf4j
 public class RangeServlet extends HttpServlet
   {
-    private static final long serialVersionUID = -387471254552805904L;
-
-    private static final int DEFAULT_BUFFER_SIZE = 64 * 1024;
+    private static final long serialVersionUID = -3874712134552805904L;
 
     @Nonnull
     private final Path rootPath;
@@ -93,139 +90,38 @@ public class RangeServlet extends HttpServlet
             return;
           }
 
-        final long length = (int)Files.size(resourcePath);
-        final Range fullRange = new Range(0, length - 1, length);
-        final List<Range> ranges = new ArrayList<>();
-
-        final String range = request.getHeader("Range");
-
-        if (range != null)
-          {
-            // Range header should match format "bytes=n-n,n-n,n-n...".
-            if (!range.matches("^bytes=\\d*-\\d*(,\\d*-\\d*)*$"))
-              {
-                log.error("Invalid range: {}", range);
-                response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
-                response.sendError(SC_REQUESTED_RANGE_NOT_SATISFIABLE);
-                return;
-              }
-
-            // If any valid If-Range header, then process each part of byte range.
-            for (final String part : range.substring(6).split(","))
-              {
-                // Assuming a file with length of 100, the following examples returns bytes at:
-                // 50-80 (50 to 80), 40- (40 to length=100), -20 (length-20=80 to length=100).
-                long start = subStringOrMinusOne(part, 0, part.indexOf("-"));
-                long end = subStringOrMinusOne(part, part.indexOf("-") + 1, part.length());
-
-                if (start == -1)
-                  {
-                    start = length - end;
-                    end = length - 1;
-                  }
-                else if ((end == -1) || (end > length - 1))
-                  {
-                    end = length - 1;
-                  }
-
-                if (start > end)
-                  {
-                    log.error("Invalid range: {}", range);
-                    response.setHeader("Content-Range", "bytes */" + length); // Required in 416.
-                    response.sendError(SC_REQUESTED_RANGE_NOT_SATISFIABLE);
-                    return;
-                  }
-
-                ranges.add(new Range(start, end, length));
-              }
-          }
+        final long length = Files.size(resourcePath);
+        final Range fullRange = Range.full(length);
+        final List<Range> ranges = Range.fromHeader(request.getHeader("Range"), length);
 
         if (ranges.size() > 1)
           {
-            log.error("Can't support multi-range: {}", range);
+            log.error("Can't support multi-range: {}", ranges);
             response.setStatus(SC_INTERNAL_SERVER_ERROR);
             return;
           }
 
         response.setContentType("audio/mpeg");
     //                response.setContentType(Files.probeContentType(resourcePath)); FIXME
-        final Range r = ranges.stream().findFirst().orElse(fullRange);
+        final Range range = ranges.stream().findFirst().orElse(fullRange);
         response.setBufferSize(0);
-        response.setContentLength((int)r.length);
+        response.setContentLength((int)range.getLength());
 
-        if (!fullRange.equals(r))
+        if (!fullRange.equals(range))
           {
-            response.setHeader("Content-Range", "bytes " + r.start + "-" + r.end + "/" + r.total);
+            response.setHeader("Content-Range", range.toHeader());
             response.setStatus(SC_PARTIAL_CONTENT);
           }
 
         try (final RandomAccessFile input = new RandomAccessFile(resourcePath.toFile(), "r");
              final OutputStream output = response.getOutputStream())
           {
-            // FIXME: use memory mapped i/o
-            copy(input, output, r.start, r.length);
+            range.copy(input, output);
             output.flush(); // force EOF in case
           }
         catch (EOFException e)
           {
             log.debug("EOF - probably client closed connection");
-          }
-      }
-
-    /**
-     * Returns a substring of the given string value from the given begin index to the given end
-     * index as a long. If the substring is empty, then -1 will be returned
-     * @param value The string value to return a substring as long for.
-     * @param beginIndex The begin index of the substring to be returned as long.
-     * @param endIndex The end index of the substring to be returned as long.
-     * @return A substring of the given string value as long or -1 if substring is empty.
-     */
-    private long subStringOrMinusOne (String value, int beginIndex, int endIndex)
-      {
-        String substring = value.substring(beginIndex, endIndex);
-        return (substring.length() > 0) ? Long.parseLong(substring) : -1;
-      }
-
-    /**
-     * Copy the given byte range of the given input to the given output.
-     * @param input The input to copy the given range to the given output for.
-     * @param output The output to copy the given range from the given input for.
-     * @param start Start of the byte range.
-     * @param length Length of the byte range.
-     * @throws IOException If something fails at I/O level.
-     */
-    private void copy (RandomAccessFile input, OutputStream output, long start, long length)
-      throws IOException
-      {
-        final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-        int read;
-
-        if (input.length() == length)
-          {
-            // Write full range.
-            while ((read = input.read(buffer)) > 0)
-              {
-                output.write(buffer, 0, read);
-              }
-          }
-        else
-          {
-            // Write partial range.
-            input.seek(start);
-            long toRead = length;
-
-            while ((read = input.read(buffer)) > 0)
-              {
-                if ((toRead -= read) > 0)
-                  {
-                    output.write(buffer, 0, read);
-                  }
-                else
-                  {
-                    output.write(buffer, 0, (int) toRead + read);
-                    break;
-                  }
-              }
           }
       }
 
