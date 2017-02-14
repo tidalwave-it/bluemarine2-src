f7e9191d2fa2adbf03fe4e7b54e29e96bb8aa3ba
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-02-14 20:51:23 +0100

    Fixed regression in deb scripts after previous refactorings.

--- modules/Installers/Service/src/main/resources/bin/bluemarine2-service.sh
+++ modules/Installers/Service/src/main/resources/bin/bluemarine2-service.sh
@@ -3,8 +3,8 @@
 BASE_DIR="$(dirname -- "$(readlink -f -- "$0")")"
 JAR_FILE=$BASE_DIR/../lib/it-tidalwave-bluemarine2-headlessservice-${project.version}.jar
 
-WORKSPACE=/var/lib/bluemarine2
-LOG_FOLDER=/var/log/bluemarine2
+WORKSPACE=/var/lib/bluemarine2-service
+LOG_FOLDER=/var/log/bluemarine2-service
 
 mkdir -p "$LOG_FOLDER"
 
