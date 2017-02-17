4a501b90a2152a302a2b5c5fab247e90960a9ede
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2017-02-17 17:29:43 +0100

    DbTune name convention fixes.

--- modules/MediaScanner/src/main/java/it/tidalwave/bluemarine2/metadata/impl/audio/embedded/EmbeddedAudioMetadataImporter.java
+++ modules/MediaScanner/src/main/java/it/tidalwave/bluemarine2/metadata/impl/audio/embedded/EmbeddedAudioMetadataImporter.java
@@ -295,7 +295,7 @@ public class EmbeddedAudioMetadataImporter
             .withOptional(newGroupIri,   BMMO.P_IMPORTED_FROM,        BMMO.O_SOURCE_EMBEDDED)
             .withOptional(newGroupIri,   RDFS.LABEL,                  literalFor(makerName))
             .withOptional(newGroupIri,   FOAF.NAME,                   literalFor(makerName))
-            .withOptional(newGroupIri,   DbTune.ARTIST_TYPE,          literalFor((short)2))
+            .withOptional(newGroupIri,   DbTune.P_ARTIST_TYPE,        literalFor((short)2))
             .withOptional(newGroupIri,   Purl.P_COLLABORATES_WITH,    artists.stream().map(Pair::getIri))
             .toModel();
       }
