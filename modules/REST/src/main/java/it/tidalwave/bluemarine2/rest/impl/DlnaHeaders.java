f373ac8d0f479a78c011571eca16c1a2eff14f10
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2021-04-10 14:48:09 +0200

    Fixed header.

--- modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/DlnaHeaders.java
+++ modules/REST/src/main/java/it/tidalwave/bluemarine2/rest/impl/DlnaHeaders.java
@@ -1,12 +1,10 @@
 /*
- * #%L
  * *********************************************************************************************************************
  *
- * blueMarine2 - Semantic Media Center
- * http://bluemarine2.tidalwave.it - git clone https://tidalwave@bitbucket.org/tidalwave/bluemarine2-src.git
- * %%
- * Copyright (C) 2015 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
- * %%
+ * blueMarine II: Semantic Media Centre
+ * http://tidalwave.it/projects/bluemarine2
+ *
+ * Copyright (C) 2015 - 2021 by Tidalwave s.a.s. (http://tidalwave.it)
  *
  * *********************************************************************************************************************
  *
@@ -21,17 +19,16 @@
  *
  * *********************************************************************************************************************
  *
- * $Id$
+ * git clone https://bitbucket.org/tidalwave/bluemarine2-src
+ * git clone https://github.com/tidalwave-it/bluemarine2-src
  *
  * *********************************************************************************************************************
- * #L%
  */
 package it.tidalwave.bluemarine2.rest.impl;
 
 /***********************************************************************************************************************
  *
- * @author  Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
- * @version $Id: $
+ * @author  Fabrizio Giudici
  *
  **********************************************************************************************************************/
 public class DlnaHeaders
